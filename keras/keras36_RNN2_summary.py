68a9c41e8a65f56f4c1f697c5a3af3f19cc1e410
kovtjw <kovtjw@naver.com>
2021-12-14 16:55:32 +0900

    first commit

---
+++ keras/keras36_RNN2_summary.py
@@ -0,0 +1,46 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN, Dropout 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+
+#1. 데이터
+x = np.array([[1,2,3],
+             [2,3,4],
+             [3,4,5],
+             [4,5,6]])
+y = np.array([4,5,6,7])
+
+print(x.shape,y.shape)  # (4, 3) (4,)
+
+
+# input_shape = (batch_size, timesteps, feature)
+# input_shape = (행, 열, 몇 개씩 자르는지!!)  >>> 2차원 데이터를 3차원으로 변경해주어야 한다. >>>> reshape는 데이터의 내용과 순서를 바꾸지 않는다.
+x = x.reshape(4, 3, 1) 
+# print(x)
+
+
+#2. 모델구성
+model = Sequential()
+model.add(SimpleRNN(20,activation='relu',input_shape = (3,1))) # input_shape 할 때 행은 넣지 않는다. 
+model.add(Dense(32, activation='relu'))       # 모든 activation은 다음으로 보내는 값을 한정 시키기 때문에 윗 줄에서도 가능하다.
+model.add(Dense(16))
+model.add(Dense(8))
+model.add(Dense(1))
+
+model.summary()
+'''
+recurrent_weights + input_weights + biases
+(unit 개수*unit 개수) + (input_dim(feature)수 * unit 개수) + (1 * unit 개수)
+10*10= 100 + 10 + 10
+dense 넘어가면, unit 개수 * dense 값 + bias
+'''
+
+
+# #3. 컴파일, 훈련 
+# model.compile(loss = 'mae', optimizer = 'adam')   # optimizer는 loss값을 최적화 한다.
+# model.fit(x, y, epochs = 300)
+
+# #4. 평가, 예측 
+# model.evaluate(x, y)
+# result = model.predict([[[5],[6],[7]]])
+# print(result)
---
+++ keras/keras38_LSTM2_summary.py
@@ -0,0 +1,44 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN, LSTM
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+
+#1. 데이터
+x = np.array([[1,2,3],
+             [2,3,4],
+             [3,4,5],
+             [4,5,6]])
+y = np.array([4,5,6,7])
+
+print(x.shape,y.shape)  # (4, 3) (4,)
+
+
+# input_shape = (batch_size, timesteps, feature)
+# input_shape = (행, 열, 몇 개씩 자르는지!!)  >>> 2차원 데이터를 3차원으로 변경해주어야 한다. >>>> reshape는 데이터의 내용과 순서를 바꾸지 않는다.
+x = x.reshape(4, 3, 1) 
+# print(x)
+
+
+#2. 모델구성
+model = Sequential()
+model.add(LSTM(32,activation='relu',input_shape = (3,1))) # input_shape 할 때 행은 넣지 않는다. 
+model.add(Dense(64, activation='relu'))       # 모든 activation은 다음으로 보내는 값을 한정 시키기 때문에 윗 줄에서도 가능하다.
+model.add(Dense(32))
+model.add(Dense(16))
+model.add(Dense(8))
+model.add(Dense(1))
+
+model.summary()
+
+# #3. 컴파일, 훈련 
+# model.compile(loss = 'mse', optimizer = 'adam')   # optimizer는 loss값을 최적화 한다.
+# model.fit(x, y, epochs = 200)
+
+# #4. 평가, 예측 
+# model.evaluate(x, y)
+# result = model.predict([[[5],[6],[7]]])
+# print(result)
+
+'''
+
+'''
