35e3d1ee3e8924ce200e1f4e2fe3a7f3825ca456
kovtjw <kovtjw@naver.com>
2021-12-22 09:28:36 +0900

    fitst commit

--- keras/samsungkium_6_siga.py
+++ keras/samsungkium_6_siga.py
@@ -10,8 +10,8 @@ path = '../_data/juga predict/'
 samsung = pd.read_csv(path +"삼성전자.csv", thousands=',')
 kium = pd.read_csv(path + "키움증권.csv", thousands=',')
 
-samsung = samsung.drop(range(19, 1120), axis=0)
-kium = kium.drop(range(19, 1060), axis=0)
+samsung = samsung.drop(range(13, 1120), axis=0)
+kium = kium.drop(range(13, 1060), axis=0)
 
 samsung = samsung.loc[::-1].reset_index(drop=True)
 kium = kium.loc[::-1].reset_index(drop=True)
@@ -56,25 +56,25 @@ x1_train, x1_test, y1_train, y1_test = train_test_split(x1,y1,
 x2_train, x2_test,y2_train,y2_test = train_test_split(x2,y2,
         train_size = 0.8, shuffle = True, random_state = 42)
 
-print(x1_train.shape,y1_train.shape,x2_train.shape,y2_train.shape)  # (874, 20, 4) (874, 20)
+# print(x1_train.shape,y1_train.shape,x2_train.shape,y2_train.shape)  # (874, 20, 4) (874, 20)
 
 
 #2-1 모델1
 input1 = Input(shape=(5,1))   # (70,2) 
 dense1 = LSTM(64, activation='tanh')(input1)
-dense2 = Dense(32, activation='relu')(dense1)
-dense3 = Dense(16, activation='relu')(dense2)
-dense4 = Dense(8, activation='relu')(dense3)
-output1 = Dense(4, activation='relu')(dense4)
+dense2 = Dense(64, activation='relu')(dense1)
+dense3 = Dense(32, activation='relu')(dense2)
+dense4 = Dense(16, activation='relu')(dense3)
+output1 = Dense(28, activation='relu')(dense4)
 
 
 #2-2 모델2
 input2 = Input(shape=(5,1))   # (70,3) 
 dense11 = LSTM(64, activation='tanh')(input2)
-dense12 = Dense(32, activation='relu')(dense11)
-dense13 = Dense(16, activation='relu')(dense12)
-dense14 = Dense(8, activation='relu')(dense13)
-output2 = Dense(4, activation='relu')(dense14)
+dense12 = Dense(64, activation='relu')(dense11)
+dense13 = Dense(32, activation='relu')(dense12)
+dense14 = Dense(16, activation='relu')(dense13)
+output2 = Dense(28, activation='relu')(dense14)
 
 from tensorflow.keras.layers import Concatenate, concatenate  
 merge1 = Concatenate()([output1,output2]) 
@@ -109,14 +109,14 @@ model.compile(loss='mae', optimizer = 'adam')
 
 
 
-model.fit([x1_train,x2_train], [y1_train,y2_train], epochs=1000, batch_size=1,
+model.fit([x1_train,x2_train], [y1_train,y2_train], epochs=500, batch_size=1,
           validation_split=0.2)
 
 
-# print("걸린시간 : ", round(end, 3), '초')
-# model = load_model('./_ModelCheckPoint/samsung_kium_1221_2038_0028-3946.2766.hdf5')
-
-model.save('../_test/_save/siga2.h5')
+# # print("걸린시간 : ", round(end, 3), '초')
+# # model = load_model('./_ModelCheckPoint/samsung_kium_1221_2038_0028-3946.2766.hdf5')
+# model = load_model ('../_test/_save/siga1.h5')
+model.save('../_test/_save/siga1.h5')
 #4. 평가, 예측
 loss = model.evaluate ([x1_test, x2_test], [y1_test,y2_test], batch_size=1)
 print('loss :', loss) #loss :
@@ -128,14 +128,3 @@ y1_pred, y2_pred = model.predict([result1,result2])
 
 print('삼성 시가 예측값 : ', y1_pred[-1][-1])
 print('키움 시가 예측값 : ', y2_pred[-1][-1])
-
-'''
-삼성 시가 예측값 :  78087.75
-키움 시가 예측값 :  109364.26
-
-삼성 시가 예측값 :  78038.38
-키움 시가 예측값 :  109664.27
-
-삼성 시가 예측값 :  78189.8
-키움 시가 예측값 :  109161.46
-'''
