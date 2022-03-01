1e73fe563882c32a5010c77b7b58adf60473956e
kovtjw <kovtjw@naver.com>
2022-03-01 22:53:50 +0900

    first commit

--- TEAM/easyocr_test_1.py
+++ TEAM/easyocr_test_1.py
@@ -3,5 +3,7 @@ import easyocr
 reader = easyocr.Reader(['en','ko'])
 results = reader.readtext('0CFB2A61F0E3AB6B6C27F89E07257201.jpg')
 
+reader1 = easyocr.Reader(['en','ko'])
+results1 = reader1.readtext('')
 print(results)
 
