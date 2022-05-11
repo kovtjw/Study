64f4ef9ab058c6ec59f6e37a47f9055dbe71c209
kovtjw <kovtjw@naver.com>
2022-05-11 12:12:08 +0900

    first commit

--- Torch/torch02_deep.py
+++ Torch/torch02_deep.py
@@ -1,5 +1,3 @@
-# gpu
-# criterion 이슈
 import numpy as np 
 import torch
 import torch.nn as nn
@@ -11,7 +9,6 @@ USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
 print('torch : ', torch.__version__, '사용 DEVICE :',DEVICE)
 
-
 #1. 데이터 (정제된 데이터)
 x = np.array([1,2,3])
 y = np.array([1,2,3])
@@ -20,27 +17,26 @@ y = np.array([1,2,3])
 x = torch.FloatTensor(x).unsqueeze(1).to(DEVICE)   # (3,) -> (3,1) 
 y = torch.FloatTensor(y).unsqueeze(1).to(DEVICE)   # (3,) -> (3,1)
 
-# unsqueeze( ):
-
 print(x,y)
 print(x.shape, y.shape)
 
 #2. 모델구성
-# model = Sequential()
-# model.add(Dense(1, input_dim=1)) # 데이터 세트에 하나를 넣어서 하나를 얻겠다.
-# Dense는 밀집된 레이어의 신경망 구조 
-
-model = nn.Linear(1, 1).to(DEVICE)     # 인풋, 아웃풋 / x의 1, y의 1
+# model = nn.Linear(1, 1).to(DEVICE)     # 인풋, 아웃풋 / x의 1, y의 1
 # (3,) - > (3, 1) y값도 행렬 형태로 사용해야 한다. 
+### deep하게 구성해보기!
+model = nn.Sequential(
+    nn.Linear(1, 5),
+    nn.Linear(5, 3),
+    nn.Linear(3, 4),
+    nn.Linear(4, 2),
+    nn.Linear(2, 1),
+).to(DEVICE)
+
 
 #3. 컴파일, 훈련
-# model.compile(loss='mae', optimizer='adam') 
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-
-
-# model.fit(x, y, epochs=2500, batch_size=2) 
 def train(model, criterion, optimizer, x, y):
     # model.train()        # 훈련모드 
     optimizer.zero_grad()  # 기울기 초기화 : 반드시 넣어야 한다. 
@@ -55,7 +51,6 @@ def train(model, criterion, optimizer, x, y):
     loss.backward()       # 기울기 값 계산까지다 
     optimizer.step()      # 가중치 수정
     return loss.item()    # 사람이 보기 좋게 만들어주기
-
 
 epochs = 500
 for epoch in range(1, epochs+1):
--- Torch/torch03_mlp2.py
+++ Torch/torch03_mlp2.py
@@ -1,5 +1,3 @@
-# gpu
-# criterion 이슈
 import numpy as np 
 import torch
 import torch.nn as nn
@@ -11,51 +9,52 @@ USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
 print('torch : ', torch.__version__, '사용 DEVICE :',DEVICE)
 
-
 #1. 데이터 (정제된 데이터)
-x = np.array([1,2,3])
-y = np.array([1,2,3])
+x = np.array([[1,2,3,4,5,6,7,8,9,10],
+             [1,1.1,1.2,1.3,1.4,1.5,
+             1.6,1.5,1.4,1.3],
+             [10, 9,8,7,6,5,4,3,2,1]])
+x = np.transpose(x)  #(2,10) - > (10,2)
+y = np.array([11,12,13,14,15,16,17,18,19,20])
 
 # 텐서형 데이터로 변환해줘야 함
-x = torch.FloatTensor(x).unsqueeze(1).to(DEVICE)   # (3,) -> (3,1) 
+x = torch.FloatTensor(x).to(DEVICE)   # (3,) -> (3,1) 
 y = torch.FloatTensor(y).unsqueeze(1).to(DEVICE)   # (3,) -> (3,1)
 
-# unsqueeze( ):
-
 print(x,y)
-print(x.shape, y.shape)
+print(x.shape, y.shape)   # torch.Size([10, 2]) torch.Size([10, 1])
 
 #2. 모델구성
-# model = Sequential()
-# model.add(Dense(1, input_dim=1)) # 데이터 세트에 하나를 넣어서 하나를 얻겠다.
-# Dense는 밀집된 레이어의 신경망 구조 
-
-model = nn.Linear(1, 1).to(DEVICE)     # 인풋, 아웃풋 / x의 1, y의 1
+# model = nn.Linear(1, 1).to(DEVICE)     # 인풋, 아웃풋 / x의 1, y의 1
 # (3,) - > (3, 1) y값도 행렬 형태로 사용해야 한다. 
+### deep하게 구성해보기!
+model = nn.Sequential(
+    nn.Linear(3, 5),
+    nn.Linear(5, 3),
+    nn.Linear(3, 4),
+    nn.Linear(4, 2),
+    nn.Linear(2, 1),
+).to(DEVICE)
+
 
 #3. 컴파일, 훈련
-# model.compile(loss='mae', optimizer='adam') 
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+optimizer = optim.AdamW(model.parameters(), lr=0.001)
 
-
-
-# model.fit(x, y, epochs=2500, batch_size=2) 
 def train(model, criterion, optimizer, x, y):
     # model.train()        # 훈련모드 
     optimizer.zero_grad()  # 기울기 초기화 : 반드시 넣어야 한다. 
     
     hypothesis = model(x)
     
-    # loss = criterion(hypothesis, y)
+    loss = criterion(hypothesis, y)
     # loss = nn.MSELoss(hypothesis,y) # 에러
-    loss = nn.MSELoss()(hypothesis,y) # 가능
-    loss = F.mse_loss(hypothesis,y)
+    # loss = nn.MSELoss()(hypothesis,y) # 가능
+    # loss = F.mse_loss(hypothesis,y)
     # 여기까지가 순전파
     loss.backward()       # 기울기 값 계산까지다 
     optimizer.step()      # 가중치 수정
     return loss.item()    # 사람이 보기 좋게 만들어주기
-
 
 epochs = 500
 for epoch in range(1, epochs+1):
@@ -76,5 +75,5 @@ def evaluate(model, criterion,x, y ):
 loss2 = evaluate(model, criterion, x,y)
 print('최종 loss : ', loss2)
 
-result = model(torch.Tensor([[4]]).to(DEVICE))
+result = model(torch.Tensor([[(10,1.3,1)]]).to(DEVICE))
 print('4의 예측값 :', result)
