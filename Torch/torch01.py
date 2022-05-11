f901c41cf40b456ce678bba8ee40ad79fd07be04
kovtjw <kovtjw@naver.com>
2022-05-11 11:15:45 +0900

    first commit

--- Torch/torch01.py
+++ Torch/torch01.py
@@ -9,8 +9,11 @@ import torch.nn.functional as F
 x = np.array([1,2,3])
 y = np.array([1,2,3])
 
-x = torch.FloatTensor(x) # 텐서형 데이터로 변환해줘야 함
-y = torch.FloatTensor(y)
+# 텐서형 데이터로 변환해줘야 함
+x = torch.FloatTensor(x).unsqueeze(1)   # (3,) -> (3,1) 
+y = torch.FloatTensor(y).unsqueeze(1)   # (3,) -> (3,1)
+
+# unsqueeze( ):
 
 print(x,y)
 print(x.shape, y.shape)
@@ -18,22 +21,60 @@ print(x.shape, y.shape)
 #2. 모델구성
 # model = Sequential()
 # model.add(Dense(1, input_dim=1)) # 데이터 세트에 하나를 넣어서 하나를 얻겠다.
+# Dense는 밀집된 레이어의 신경망 구조 
 
-model = nn.Linear(1, 1)
+model = nn.Linear(1, 1)     # 인풋, 아웃풋 / x의 1, y의 1
+# (3,) - > (3, 1) y값도 행렬 형태로 사용해야 한다. 
 
 #3. 컴파일, 훈련
-model.compile(loss='mae', optimizer='adam') # mse = 평균 제곱 에러 > loss 값은 작으면 좋다. / optimizer = 'adam' -> loss의 mse를 감축시키는 역할을 한다.
+# model.compile(loss='mae', optimizer='adam') 
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-model.fit(x, y, epochs=2500, batch_size=2) # epochs = 훈련량 / batch_size = 몇개씩 잘라서 훈련 시킬 것인지, 배치가 작을 수록 훈련이 잘된다. -> 데이터 양이 많을수록 오래걸리고 오류가 생김
+
+
+# model.fit(x, y, epochs=2500, batch_size=2) 
+def train(model, criterion, optimizer, x, y):
+    # model.train()        # 훈련모드 
+    optimizer.zero_grad()  # 기울기 초기화 : 반드시 넣어야 한다. 
+    
+    hypothesis = model(x)
+    
+    loss = criterion(hypothesis, y)
+    # 여기까지가 순전파
+    loss.backward()       # 기울기 값 계산까지다 
+    optimizer.step()      # 가중치 수정
+    return loss.item()    # 사람이 보기 좋게 만들어주기
+
+
+epochs = 500
+for epoch in range(1, epochs+1):
+    loss = train(model, criterion, optimizer, x, y)
+    print('epoch : {}, loss: {}'.format(epoch, loss))
+    
+print('========================================================')
 
 #4. 평가, 예측 
-loss = model.evaluate(x,y) # fit 이후 저장되어있는 훈련값(데이터)
-print('loss :', loss)
-result = model.predict([4])
+def evaluate(model, criterion,x, y ):
+    model.eval()    # 평가모드
+    
+    with torch.no_grad():
+        predict = model(x)
+        loss = criterion(predict, y)
+    return loss.item()
+    
+loss2 = evaluate(model, criterion, x,y)
+print('최종 loss : ', loss2)
+
+result = model(torch.Tensor([[4]]))
 print('4의 예측값 :', result)
 
+# loss = model.evaluate(x,y) # fit 이후 저장되어있는 훈련값(데이터)
+# print('loss :', loss)
+# result = model.predict([4])
+# print('4의 예측값 :', result)
+
 '''
-최적의 결과 값 기입하기
-loss : 0.0
-4의 예측값 : [[4.]]
+최종 loss :  4.952517032623291
+4의 예측값 : tensor([[-0.4868]], grad_fn=<AddmmBackward0>)
 '''
